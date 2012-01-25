3b3cfd28e7b72bf2a424880c9b5cc1590bec4e45
Grant Cox <grant.cox@gmail.com>
2012-01-24 19:40:57 -0500

    Finds and prints the native and foreign titles and subtitles

--- read.py
+++ read.py
@@ -7,23 +7,30 @@ f = open('verbos.b4u', 'rb')
 contents = f.read()
 f.close()
  
-# helper function to simplify reading syntax
 def r(fmt, offset):
 	read = struct.unpack_from('<' + fmt, contents, offset)
 	if len(read) == 1:
 		return read[0]
 	return read
  
-# the Windows console fails on non-ascii characters, so we also log to a file
+def s(offset):
+	s = u''
+	length = r('H', offset)
+	for i in range(length):
+		raw = r('H', offset + i*2 +2)
+		char = raw ^ 0x7E
+		s = s + unichr(char)
+	return s
+ 
 logfile = codecs.open('log.txt', encoding='utf-8', mode='a')
 def log(s):
 	s = unicode(s)
-	print s.encode('ascii', 'ignore') # only send ascii to the console
-	logfile.write('\n' + s) # but write the full content to file
+	print s.encode('ascii', 'ignore')
+	logfile.write('\n' + s)
 log('---- ' + str(datetime.now()) + ' -----------------')
  
  
-# read the .b4u deck
+# read the file
 caret = 144
 boundary, card_count = r('LL', caret)
 next_card = r('L', caret +16)
@@ -32,5 +39,12 @@ log('found ' + str(card_count) + ' cards')
  
 while (next_card != 0):
 	address = next_card
-	next_card, card_num, boundary, a, b = r('LLLLL', next_card)
-	log('card ' + str(card_num) + ' @' + str(address) + ' a: ' + str(a) + ' b: ' + str(b))
+	next_card, card_num, boundary, card_data, b = r('LLLLL', next_card)
+	log('card ' + str(card_num) + ' @' + str(address) + ' data: ' + str(card_data) + ' b: ' + str(b))
+ 
+	card_head, number, english_title, english_subtitle, foreign_title, foreign_subtitle = r('LLLLLL', card_data)
+	english_title = s(english_title)
+	english_subtitle = s(english_subtitle)
+	foreign_title = s(foreign_title)
+	foreign_subtitle = s(foreign_subtitle)
+	log('  ' + foreign_title + ' : ' + foreign_subtitle + ' => ' + english_title + ' : ' + english_subtitle)
